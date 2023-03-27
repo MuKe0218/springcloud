69816ce13c140c4a670d1958412b212573f594ca
kql <769156974@qq.com>
2023-03-27 16:13:59 +0800

    eureka集群

---
+++ springcloud-eureka-7002/src/main/java/com/kql/EurekaService_7002.java
@@ -0,0 +1,13 @@
+package com.kql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //服务端的启动类，可以
+public class EurekaService_7002 {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaService_7002.class,args);
+    }
+}
